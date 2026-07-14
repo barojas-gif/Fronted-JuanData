8cbeca9ce4ba635979326a2e736ba75da3962933
Brian Andres <brianandres54321@gmail.com>
2026-07-14 10:01:00 -0500

    :ghost: Fix: Ajuste de rutas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    apiBaseUrl: 'http://localhost:8080/api',
-    uploadsBaseUrl: 'http://localhost:8080/api/documentos/uploads/'
+    production: true,
+    apiBaseUrl: 'https://backend-juan-data.onrender.com/api',
+    uploadsBaseUrl: 'https://backend-juan-data.onrender.com/api/documentos/uploads/'
 };
