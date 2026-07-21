347c1c1a86180b509c8edc21f262f8f0e19fd4c4
Brian Andres <brianandres54321@gmail.com>
2026-07-21 09:27:51 -0500

    :ghost: Fix: Se reajusto el proyecto

---
+++ src/environments/environment.prod.example.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://YOUR_BACKEND_URL/api',
+    uploadsBaseUrl: 'https://YOUR_BACKEND_URL/api/documentos/uploads/'
+};
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiBaseUrl: 'https://backend-juan-data.onrender.com/api',
-    uploadsBaseUrl: 'https://backend-juan-data.onrender.com/api/documentos/uploads/'
-};
